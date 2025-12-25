1ba6ffd14b84a48140bb611b94f81453ea2d773a
aitwehrrg <rrginfinity@gmail.com>
2025-12-25 23:31:38 +0530

    Updated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const ibmPlexSans = IBM_Plex_Sans({
+    weight: ["400", "500", "600", "700"],
+    subsets: ["latin"],
+    variable: "--font-plex-sans",
+    display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,10 +32,10 @@ export default function RootLayout({
   return (
       <html lang="en">
           <body
-              className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+              className={`cmu-tt bg-neutral-50 text-neutral-900 antialiased`}
           >
               {children}
-              <footer className="flex items-center justify-center py-4">
+              <footer className="flex items-center justify-center pb-4">
                   <p className="text-sm text-muted-foreground">
                       Built by{" "}
                       <a
