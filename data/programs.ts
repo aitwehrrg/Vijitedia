7b4adefe753836ebb4ef75a4ffc3b93b90660089
RRG <rrginfinity@gmail.com>
2026-01-01 18:40:51 +0000

    Added ExTC courses

--- data/programs.ts
+++ data/programs.ts
@@ -1,6 +1,8 @@
 import { Program } from "@/types/flowsheet";
 import { CE_PROGRAM } from "./majors/btech-ce";
 import { IT_PROGRAM } from "./majors/btech-it";
-import { EL_PROGRAM } from "./majors/btech-el";
 
-export const FLOWSHEET_DATA: Program[] = [CE_PROGRAM, IT_PROGRAM, EL_PROGRAM];
+import { EL_PROGRAM } from "./majors/btech-el";
+import { ET_PROGRAM } from "./majors/btech-et";
+
+export const FLOWSHEET_DATA: Program[] = [CE_PROGRAM, IT_PROGRAM, EL_PROGRAM, ET_PROGRAM];
