b4f1e3138e53c596db29303c42b7516788cc291d
RRG <rrginfinity@gmail.com>
2026-01-02 09:22:35 +0000

    Added Electrical Engineering

--- data/programs.ts
+++ data/programs.ts
@@ -2,7 +2,14 @@ import { Program } from "@/types/flowsheet";
 import { CE_PROGRAM } from "./majors/btech-ce";
 import { IT_PROGRAM } from "./majors/btech-it";
 
+import { EE_PROGRAM } from "./majors/btech-ee";
 import { EL_PROGRAM } from "./majors/btech-el";
 import { ET_PROGRAM } from "./majors/btech-et";
 
-export const FLOWSHEET_DATA: Program[] = [CE_PROGRAM, IT_PROGRAM, EL_PROGRAM, ET_PROGRAM];
+export const FLOWSHEET_DATA: Program[] = [
+    CE_PROGRAM,
+    IT_PROGRAM,
+    EE_PROGRAM,
+    EL_PROGRAM,
+    ET_PROGRAM,
+];
