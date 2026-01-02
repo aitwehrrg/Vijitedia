00ac170631976da02ffb31672382a0eda0f80360
RRG <rrginfinity@gmail.com>
2026-01-02 12:16:50 +0000

    Semantic ordering

--- data/programs.ts
+++ data/programs.ts
@@ -1,15 +1,15 @@
 import { Program } from "@/types/flowsheet";
-import { CE_PROGRAM } from "./majors/btech-ce";
-import { IT_PROGRAM } from "./majors/btech-it";
 
 import { EE_PROGRAM } from "./majors/btech-ee";
 import { EL_PROGRAM } from "./majors/btech-el";
+import { CE_PROGRAM } from "./majors/btech-ce";
+import { IT_PROGRAM } from "./majors/btech-it";
 import { ET_PROGRAM } from "./majors/btech-et";
 
 export const FLOWSHEET_DATA: Program[] = [
-    CE_PROGRAM,
-    IT_PROGRAM,
     EE_PROGRAM,
     EL_PROGRAM,
+    CE_PROGRAM,
+    IT_PROGRAM,
     ET_PROGRAM,
 ];
