768c8bdf4f01c7d1a40c1a190ad99b0d9a4146f5
RRG <rrginfinity@gmail.com>
2026-01-04 16:55:28 +0000

    Migrated utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,34 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function toRoman(num: number) {
+    const roman = [
+        "",
+        "I",
+        "II",
+        "III",
+        "IV",
+        "V",
+        "VI",
+        "VII",
+        "VIII",
+        "IX",
+        "X",
+    ];
+    return roman[num];
+}
+
+export function getSuffix(courseIndex: number) {
+    let suffix = " ";
+    if (courseIndex === 7) {
+        suffix += "Project";
+        return suffix;
+    }
+    suffix += toRoman((courseIndex >> 1) + 1);
+    if ((courseIndex & 1) === 1) suffix += " Laboratory";
+    return suffix;
 }
